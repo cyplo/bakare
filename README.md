3c8a8cd57a91d967f0d7598921b0dd3475dcede8
Cyryl Płotnicki <cyplo@cyplo.net>
2018-08-18 18:39:40 +0100

    Scaffolding of a test

--- README.md
+++ README.md
@@ -4,4 +4,9 @@ Goals:
 * memory usage limit
 * encryption by default - asymmetric, creates a keypair for you
 * deduplication
+* fuzzy find by file name in stored files
 
+Implementation:
+* hash -> file and file -> hash indexes
+* use vfs to store both db and data files, create a new one when old one too big
+* start with simple read file -> hash -> encrypt -?> send
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
-fn main() {
-    println!("Hello, world!");
-}
 
-#[cfg(test)]
-mod should {
 
-    #[test]
-    fn hash_scanned_files(){
+mod index {
+    use super::*;
+
+    #[cfg(test)]
+    mod should {
+
+        #[test]
+        fn recognize_files_of_same_contents() {
+            let file_source = FileSource::new();
+
+            file_source.add("file name 1", "file contents 1");
+            file_source.add("file name 2", "file contents 1");
+            file_source.add("file name 3", "file contents 3");
+
+            let contents_hasher = FakeContentsHasher::default();
+            
+
+            let index = Index::with(file_source, contents_hasher);
+
+
+        }
 
     }
 
