5406fcdca19822d3e74a27ae4cfcedc592174cb6
Cyryl Płotnicki <cyplo@cyplo.net>
2018-09-01 09:44:13 +0100

    WIP compiling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,47 @@
+extern crate tempfile;
 
+mod rustback {
+
+    use super::*;
+
+    #[cfg(test)]
+    mod should {
+
+        use super::*;
+        use tempfile::tempdir;
+        use std::fs::File;
+        use std::io::{self, Write};
+        use std::io::Error;
+
+        #[test]
+        fn be_able_to_restore_backed_up_files() -> Result<(), Error> {
+
+            let dir = tempdir()?;
+
+            let file_path = dir.path().join("my-temporary-note.txt");
+            let mut file = File::create(file_path)?;
+            writeln!(file, "Brian was here. Briefly.")?;
+
+            // create a new temp folder
+            // add 3 files, two identical content
+            // remember file hashes
+            let source_path = dir.path();
+
+            //create a new temp folder
+            let destination_path = "";
+            //let walker = FilesystemWalker::new(source_path);
+            //let engine = Engine::with(walker);
+
+            //engine.backup();
+            //engine.restore(destination_path);
+
+            // assert on number and hashes of files
+            Ok(())
+        }
+
+    }
+
+}
 
 mod index {
     use super::*;
@@ -8,18 +51,7 @@ mod index {
 
         #[test]
         fn recognize_files_of_same_contents() {
-            let file_source = FileSource::new();
-
-            file_source.add("file name 1", "file contents 1");
-            file_source.add("file name 2", "file contents 1");
-            file_source.add("file name 3", "file contents 3");
-
-            let contents_hasher = FakeContentsHasher::default();
-            
-
-            let index = Index::with(file_source, contents_hasher);
-
-
+            assert!(false);
         }
 
     }
