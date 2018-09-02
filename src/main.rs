4542fa5cd0d1d86ca72070e59e5e946d06d0b9f3
Cyryl Płotnicki <cyplo@cyplo.net>
2018-09-02 14:43:17 +0100

    add walker

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,40 @@
 extern crate crypto;
-extern crate tempfile;
 extern crate dir_diff;
+extern crate tempfile;
+extern crate walkdir;
 
 use std::path::Path;
+use walkdir::DirEntry;
+use walkdir::Error;
+use walkdir::WalkDir;
 
-struct BackupEngine;
-impl BackupEngine {
-    fn new(source_path: &Path, repository_path: &Path) -> Self {
-        BackupEngine {}
+struct BackupEngine<'a> {
+    source_path: &'a Path,
+    repository_path: &'a Path,
+}
+
+impl<'a> BackupEngine<'a> {
+    fn new(source_path: &'a Path, repository_path: &'a Path) -> Self {
+        BackupEngine {
+            source_path,
+            repository_path,
+        }
     }
 
-    fn backup(&self) {}
+    fn backup(&self) -> Result<(), Error> {
+        let walker = WalkDir::new(self.source_path);
+        for maybe_entry in walker {
+            match maybe_entry {
+                Ok(entry) => self.process_entry(entry),
+                Err(error) => return Err(error),
+            }
+        }
+        Ok(())
+    }
+
+    fn process_entry(&self, entry: DirEntry) {
+        println!("{:?}", entry.path());
+    }
 }
 
 struct RestoreEngine;
@@ -30,14 +54,14 @@ mod rustback {
     mod should {
 
         use super::*;
+        use dir_diff::is_different;
+        use std::fs::write;
         use std::fs::File;
         use std::io::Error;
         use std::io::{self, Write};
         use tempfile::tempdir;
         use tempfile::tempfile_in;
         use tempfile::TempDir;
-        use dir_diff::is_different;
-        use std::fs::write;
 
         #[test]
         fn be_able_to_restore_backed_up_files() -> Result<(), Error> {
@@ -55,11 +79,11 @@ mod rustback {
             let restore_engine = RestoreEngine::new(&repository.path(), &restore_target.path());
             restore_engine.restore();
 
-            let is_source_and_destination_different = is_different(&source.path(), &restore_target.path()).unwrap();
-            assert!(!is_source_and_destination_different);
+            let are_source_and_target_different =
+                is_different(&source.path(), &restore_target.path()).unwrap();
+            assert!(!are_source_and_target_different);
             Ok(())
         }
     }
 
 }
-
