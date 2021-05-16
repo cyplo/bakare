6ba70453011e1283939d1ba3d44b7efc4e01e0eb
Cyryl Płotnicki <cyplo@cyplo.net>
2021-05-16 10:37:23 +0100

    encode small amounts

--- src/io/error_correcting_encoder.rs
+++ src/io/error_correcting_encoder.rs
@@ -1,11 +1,54 @@
-use anyhow::*;
+use std::io::Read;
 
+use anyhow::*;
+use reed_solomon::Encoder;
+use reed_solomon::{Buffer, Decoder};
+
+const BLOCK_SIZE: usize = 256;
+const ECC_LENGTH: usize = 8;
+
+// TODO: make the API streaming friendly
 pub fn encode(bytes: &[u8]) -> Result<Vec<u8>> {
-    Ok(Vec::from(bytes))
+    let encoder = Encoder::new(ECC_LENGTH);
+
+    let encoded_blocks = bytes
+        .chunks(BLOCK_SIZE)
+        .map(|chunk| encoder.encode(chunk))
+        .collect::<Vec<Buffer>>();
+
+    let mut result = vec![];
+
+    dbg!(encoded_blocks.len());
+    for buffer in encoded_blocks {
+        dbg!(buffer.bytes().count());
+        for byte in buffer.bytes() {
+            result.push(byte?);
+        }
+    }
+
+    dbg!(result.len());
+    Ok(result)
 }
 
 pub fn decode(bytes: &[u8]) -> Result<Vec<u8>> {
-    Ok(Vec::from(bytes))
+    let decoder = Decoder::new(ECC_LENGTH);
+    let decoded_blocks = bytes
+        .chunks(BLOCK_SIZE + ECC_LENGTH)
+        .map(|chunk| decoder.correct(chunk, None).map_err(|e| anyhow!(format!("{:#?}", e))))
+        .collect::<Result<Vec<Buffer>>>()?;
+
+    let mut result = vec![];
+
+    dbg!(decoded_blocks.len());
+    for buffer in decoded_blocks {
+        dbg!(buffer.bytes().count());
+        for byte in buffer.data() {
+            result.push(byte.clone());
+        }
+    }
+
+    dbg!(result.len());
+    Ok(result)
 }
 
 #[cfg(test)]
@@ -19,7 +62,29 @@ mod must {
     use pretty_assertions::assert_eq;
 
     #[test]
-    #[ignore = "wip"]
+    fn encode_small_amounts_of_data() -> Result<()> {
+        let mut original: [u8; 32] = [0; 32];
+        thread_rng().fill_bytes(&mut original);
+
+        let decoded = decode(&encode(&original)?)?;
+
+        assert_eq!(decoded, original);
+
+        Ok(())
+    }
+    #[test]
+    fn encode_large_amounts_of_data() -> Result<()> {
+        let mut original: [u8; 1024 * 1024] = [0; 1024 * 1024];
+        thread_rng().fill_bytes(&mut original);
+
+        let decoded = decode(&encode(&original)?)?;
+
+        assert_eq!(decoded, original);
+
+        Ok(())
+    }
+
+    #[test]
     fn survive_data_corruption() -> Result<()> {
         let mut original: [u8; 32] = [0; 32];
         thread_rng().fill_bytes(&mut original);
@@ -32,7 +97,7 @@ mod must {
         let mut corrupted = Vec::from(encoded);
         corrupted[corrupt_byte_index] = rand::thread_rng().gen::<u8>();
 
-        let decoded = decode(&corrupted).unwrap();
+        let decoded = decode(&corrupted)?;
 
         assert_eq!(decoded, original);
 
