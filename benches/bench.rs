4730f6e0c9189c98691ce9a62d388ac6e215a88e
Cyryl Płotnicki <cyplo@cyplo.net>
2021-05-16 20:38:05 +0100

    add empty bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,1 +1,8 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("ecc", |b| b.iter(|| true));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
