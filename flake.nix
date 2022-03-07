a2eb830dd5970e79f0a085b637514ac8093e79e2
Cyryl Płotnicki <cyplo@cyplo.net>
2022-03-07 23:05:20 +0000

    update, start adding cli

--- flake.nix
+++ flake.nix
@@ -1,95 +1,55 @@
 {
-  description = "A simple yet robust commandline random password generator.";
-
+  description = "Bakare: modern and simple, yet efficient backup solution";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
     utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    crate2nix = {
-      url = "github:kolloch/crate2nix";
-      flake = false;
-    };
+    naersk.url = "github:nix-community/naersk";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, utils, rust-overlay, crate2nix, ... }:
-  let
-    name = "bakare";
-    rustChannel = "stable";
-  in
-  utils.lib.eachDefaultSystem
-  (system:
-  let
-          # Imports
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              rust-overlay.overlay
-              (self: super: {
-                # Because rust-overlay bundles multiple rust packages into one
-                # derivation, specify that mega-bundle here, so that crate2nix
-                # will use them automatically.
-                rustc = self.rust-bin.${rustChannel}.latest.default;
-                cargo = self.rust-bin.${rustChannel}.latest.default;
-              })
-            ];
-          };
-          inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
-          generatedCargoNix;
+  outputs = { self, nixpkgs, utils, naersk, flake-compat }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        naersk-lib = naersk.lib."${system}";
+      in
+      rec {
+        # `nix build`
+        packages.bakare = naersk-lib.buildPackage {
+          pname = "bakare";
+          root = ./.;
+        };
+        defaultPackage = packages.bakare;
 
-          # Create the cargo2nix project
-          project = pkgs.callPackage
-          (generatedCargoNix {
-            inherit name;
-            src = ./.;
-          })
-          {
-              # Individual crate overrides go here
-              # Example: https://github.com/balsoft/simple-osd-daemons/blob/6f85144934c0c1382c7a4d3a2bbb80106776e270/flake.nix#L28-L50
-              defaultCrateOverrides = pkgs.defaultCrateOverrides // {
-                # The himalaya crate itself is overriden here. Typically we
-                # configure non-Rust dependencies (see below) here.
-                ${name} = oldAttrs: {
-                  inherit buildInputs nativeBuildInputs;
-                };
-              };
-            };
+        # `nix run`
+        apps.bakare = utils.lib.mkApp {
+          drv = packages.bakare;
+        };
+        defaultApp = apps.bakare;
 
-            buildInputs = with pkgs; [ openssl.dev cacert openssh zlib ];
-            nativeBuildInputs = with pkgs; [ rustc cargo pkgconfig git ];
-  in
-  rec {
-    packages.${name} = project.rootCrate.build;
-
-          # `nix build`
-          defaultPackage = packages.${name};
-
-          # `nix run`
-          apps.${name} = utils.lib.mkApp {
-            inherit name;
-            drv = packages.${name};
-          };
-          defaultApp = apps.${name};
-
-          # `nix develop`
-          devShell = pkgs.mkShell
-          {
-            inputsFrom = builtins.attrValues self.packages.${system};
-            buildInputs = buildInputs ++ (with pkgs;
-            [
-              nixpkgs-fmt
-              cargo-watch
-              cargo-edit
-              cargo-outdated
-              pkgs.rust-bin.${rustChannel}.latest.rust-analysis
-              pkgs.rust-bin.${rustChannel}.latest.rls
-            ]);
-            RUST_SRC_PATH = "${pkgs.rust-bin.${rustChannel}.latest.rust-src}/lib/rustlib/src/rust/library";
-          };
-        }
-        );
-      }
-
+        # `nix develop`
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            cacert
+            cargo
+            cargo-edit
+            cargo-outdated
+            cargo-release
+            cargo-tarpaulin
+            cargo-watch
+            clippy
+            git
+            llvmPackages_13.llvm
+            nixpkgs-fmt
+            openssh
+            openssl
+            pkg-config
+            rustc
+            rustfmt
+          ];
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        };
+      });
+}
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use seahorse::App;
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let app = App::new(env!("CARGO_PKG_NAME"))
+        .description(env!("CARGO_PKG_DESCRIPTION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .action(|c| println!("Hello, {:?}", c.args));
+
+    app.run(args);
+}
